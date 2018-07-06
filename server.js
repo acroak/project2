8db61aff45aca64c407a23c5d4d93d395b384f0e
acroak <acroak@live.com>
2018-07-06 10:44:48 -0400

    express is running

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req,res)=>{
+  res.send('sever is running');
+});
+app.listen(3000, ()=>{
+  console.log('Listening...');
+});
