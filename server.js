b3813e04d215a607a33d7f6a5dfe88db70d9404a
acroak <acroak@live.com>
2018-07-06 12:00:15 -0400

    mongoURI

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
-mongoose.connect(mongoUri);
+
 
 
 app.get('/', (req,res)=>{
   res.send('server is running');
 });
+
 app.listen(port, ()=>{
   console.log('Listening...');
 });
 
-
+mongoose.connect(mongoUri, {useNewUrlParser: true});
 mongoose.connection.on('open', ()=>{
   console.log('mongoose!!!!!!!!!!!!!!!!!!!!');
 });
