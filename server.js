bb46744645e91b8a56a67b1b8aef3e8dd52f9a66
acroak <acroak@live.com>
2018-07-06 11:52:23 -0400

    Mongoose and mongo connected

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+mongoose.connect(mongoUri);
+
+
 app.get('/', (req,res)=>{
   res.send('server is running');
 });
 app.listen(port, ()=>{
   console.log('Listening...');
+});
+
+
+mongoose.connection.on('open', ()=>{
+  console.log('mongoose!!!!!!!!!!!!!!!!!!!!');
 });
