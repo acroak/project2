5705d7d51eb141820334991752af50e404d41408
acroak <acroak@live.com>
2018-07-06 15:34:44 -0400

    basic new/show/edit routes set up

---
+++ models/seed.js
@@ -0,0 +1,8 @@
+let seedPlants = [
+  {
+    
+  },
+
+];
+
+module.exports = seedPlants;
--- server.js
+++ server.js
@@ -7,11 +7,30 @@ const mongoose = require('mongoose');
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 
 
-
+// GET INDEX
 app.get('/', (req,res)=>{
   res.render('index.ejs');
 });
 
+app.get('/house-plants',(req,res)=>{
+  res.render('all_plants.ejs');
+});
+
+app.get('/house-plants/new',(req,res)=>{
+  res.render('new.ejs');
+})
+app.get('/house-plants/:id', (req,res)=>{
+  res.render('show.ejs');
+});
+
+app.get('/house-plants/:id/edit',(req,res)=>{
+  res.render('edit.ejs');
+});
+
+
+
+
+// CONNECTIONS
 app.listen(port, ()=>{
   console.log('Listening...');
 });
